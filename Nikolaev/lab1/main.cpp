e347518c3feac19845c905d3ca23c3c6e55c1c6f
EFFECT322 <55045221+EFFECT322@users.noreply.github.com>
2020-10-17 13:17:57 +0300

    Add files via upload

--- Nikolaev/lab1/main.cpp
+++ Nikolaev/lab1/main.cpp
@@ -11,141 +11,74 @@ cкобки::=квадратные | круглые | фигурные
 #include <string>
 #include <iomanip>
 
-bool brackets(std::string expression, int& index);
 bool isSquareBrackets(std::string expression, int& index);
 bool isRoundBrackets(std::string expression, int& index);
 bool isFiguredBrackets(std::string expression, int& index);
 
-
-bool brackets(std::string expression, int& index)
-{
-	bool isExpression = false;
-	if (expression[index] == '+' || expression[index] == '-' || expression[index] == '0')
-	{
-		index += 1;
-		return true;
-	}
-	else if (expression[index] == '[')
-	{
-		index += 1;
-		isExpression = isSquareBrackets(expression, index);
-		if (expression[index] == ']')
-		{
-			index += 1;
-			return true;
-		}
-	}
-	else if (expression[index] == '(')
-	{
-		index += 1;
-		isExpression = isRoundBrackets(expression, index);
-		if (expression[index] == ')')
-		{
-			index += 1;
-			return true;
-		}
-	}
-	else if (expression[index] == '{')
-	{
-		index += 1;
-		isExpression = isFiguredBrackets(expression, index);
-		if (expression[index] == '}')
-		{
-			index += 1;
-			return true;
-		}
-	}
-	return isExpression;
-}
-
 bool isSquareBrackets(std::string expression, int& index)
 {
-	bool b = false;
-	if (expression[index] == '(') {
-		index += 1;
-		b = isRoundBrackets(expression, index);
-		index += 1;
-		if (expression[index] == ')')
-			return true;
-	}
-	else if (expression[index] == '{')
+	if (expression[index++] == ']')
+		return true;
+	else
+		index--;
+
+	if (expression[index++] == '+')
+		return true;
+	else
+		index--;
+
+	if (expression[index++] == '[')
 	{
-		index += 1;
-		b = isFiguredBrackets(expression, index);
-		index += 1;
-		if (expression[index] == '}')
+		if (isRoundBrackets(expression, index) && isFiguredBrackets(expression, index) && isSquareBrackets(expression, index))
 			return true;
 	}
-	else if (expression[index] == '-')
-	{
-		index += 1;
-		if (expression[index] == '0')
-		{
-			index += 1;
-			return true;
-		}
-	}
-	return b;
+
+	index--;
+	return false;
 }
 
 bool isRoundBrackets(std::string expression, int& index)
 {
-	bool b = false;
-	if (expression[index] == '{') {
-		index += 1;
-		b = isFiguredBrackets(expression, index);
-		index += 1;
-		if (expression[index] == '}')
-			return true;
-	}
-	else if (expression[index] == '[')
+	if (expression[index++] == ')')
+		return true;
+	else
+		index--;
+
+	if (expression[index++] == '-')
+		return true;
+	else
+		index--;
+
+	if (expression[index++] == '(')
 	{
-		index += 1;
-		b = isSquareBrackets(expression, index);
-		index += 1;
-		if (expression[index] == ']')
-			return true;
+		if (isFiguredBrackets(expression, index) && isSquareBrackets(expression, index) && isRoundBrackets(expression, index))
+		return true;
 	}
-	else if (expression[index] == '0')
-	{
-		index += 1;
-		if (expression[index] == '+')
-		{
-			index += 1;
-			return true;
-		}
-	}
-	return b;
+
+	index--;
+	return false;
 }
 
 bool isFiguredBrackets(std::string expression, int& index)
 {
-	bool b = false;
-	if (expression[index] == '[') {
-		index += 1;
-		b = isSquareBrackets(expression, index);
-		index += 1;
-		if (expression[index] == ']')
-			return true;
-	}
-	else if (expression[index] == '(')
+	if (expression[index++] == '}')
+		return true;
+	else
+		index--;
+
+	if (expression[index++] == '0')
+		return true;
+	else
+		index--;
+
+	if (expression[index++] == '{')
 	{
-		index += 1;
-		b = isRoundBrackets(expression, index);
-		index += 1;
-		if (expression[index] == ')')
+		if (isSquareBrackets(expression, index) && isRoundBrackets(expression, index) && isFiguredBrackets(expression, index))
 			return true;
 	}
-	else if (expression[index] == '+')
-	{
-		index += 1;
-		if (expression[index] == '-')
-		{
-			index += 1;
-			return true;
-		}
-	}
-	return b;
+
+	index --;
+	return false;
 }
 
 int main()
@@ -154,10 +87,10 @@ int main()
 	std::string expression;
 	std::string filePath;
 
-	//std::cout << "Enter fileIn path: ";
-	//std::cin >> filePath;
+	std::cout << "Enter fileIn path: ";
+	std::cin >> filePath;
 
-	std::ifstream fileIn("FileIn.txt");
+	std::ifstream fileIn(filePath);
 
 	if (!fileIn)
 	{
@@ -176,24 +109,22 @@ int main()
 	fileIn.clear();
 	fileIn.seekg(0);
 
-	//std::cout << "Enter fileOut Path: ";
-	//std::cin >> filePath;
+	std::cout << "Enter fileOut Path: ";
+	std::cin >> filePath;
 
-	std::ofstream fileOut("FileOut");
+	std::ofstream fileOut(filePath);
 
 	while (getline(fileIn, expression))
 	{
-		if (!brackets(expression, index) || expression.length() != index)
+		if (isSquareBrackets(expression, index) || isRoundBrackets(expression, index) || isFiguredBrackets(expression, index) || expression.length() == index)
 		{
-			fileOut << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "No";
-			//std::cout << " = No\n";
-			std::cout << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "No";
+			fileOut << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "Yes";
+			std::cout << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "Yes";
 		}
 		else
 		{
-			fileOut << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "Yes";
-			//std::cout << " = Yes\n";
-			std::cout << std::endl <<std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "Yes";
+			fileOut << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "No";
+			std::cout << std::endl << std::setw(offset) << std::left << expression << std::setw(offset) << std::left << "No";
 		}
 
 
