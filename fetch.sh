174e20c707fe005c67fd31f60cc6bc7ec1de37a9
JAkutenshi <jakutenshi@gmail.com>
2020-10-17 11:08:49 +0300

    Nikolaev added to the scripts

--- fetch.sh
+++ fetch.sh
@@ -4,7 +4,7 @@ git fetch zipzap666       # Звега
 git fetch sosedkov-kirill # Соседков
 git fetch Dmitrydavka     # Давыдов
 git fetch kirja1980       # Пращутинский
-#git fetch                # 
+git fetch EFFECT322       # Николаев
 #git fetch                # 
 #git fetch                # 
 #git fetch                # 
