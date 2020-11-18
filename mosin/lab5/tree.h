4d55bc433d91087c18ed432d792c4d3aef03b71c
Konstantin Mosin <54909993+KonstantM@users.noreply.github.com>
2020-11-18 21:28:04 +0300

    Update tree.h

--- mosin/lab5/tree.h
+++ mosin/lab5/tree.h
@@ -1,131 +1,135 @@
 #ifndef TREE_H
 #define TREE_H
 
-template <typename T> class Tree;
-#include "node.h"
-
 template <typename T>
 class Tree{
 public:
-    Tree();
-    void insert_node(T);
-    void render_node();
-    void write_node(std::string path);
+    Tree(T);
+    Tree<T> *insert(Tree<T> *, T);
+    void print(int = 0);
+    void write(std::ofstream &);
 
 private:
-    TreeNode<T>* insert(TreeNode<T> *, T data);
-    TreeNode<T>* insert_root(TreeNode<T>*, T data);
-    TreeNode<T>* rotate_left(TreeNode<T> *);
-    TreeNode<T>* rotate_right(TreeNode<T> *);
-    T get_size(TreeNode<T>*);
-    int update_size(TreeNode<T>*);
+    Tree<T> *insert_root(Tree<T> *, T);
+    Tree<T> *rotate_right(Tree<T> *);
+    Tree<T> *rotate_left(Tree<T> *);
+    int get_size(Tree<T> *);
+    void update_size(Tree<T> *);
 
 private:
-    TreeNode<T> *root;
+    T data;
+    int size;
+    Tree<T> *left;
+    Tree<T> *right;
 };
 
 template <typename T>
-Tree<T>::Tree() : root(nullptr) {}
+Tree<T>::Tree(T data) : data(data), size(1), left(nullptr), right(nullptr) {}
 
 template <typename T>
-void Tree<T>::insert_node(T data){
-    this->root = insert(this->root, data);
-}
-
-template <typename T>
-void Tree<T>::render_node(){
-    if(this->root){
-        this->root->print_tree();
+Tree<T> *Tree<T>::insert(Tree<T> *ptr, T data){
+    if (!ptr){
+        return new Tree<T>(data);
+    }
+    if (rand() % (ptr->size + 1) == 0){
+        std::cout << data << " random\n";
+        return insert_root(ptr, data);
+    }
+    if (ptr->data > data){
+        std::cout << data << " left\n";
+        ptr->left = insert(ptr->left, data);
     }
     else{
-        std::cout << "NULL" << std::endl;
+        std::cout << data << " right\n";
+        ptr->right = insert(ptr->right, data);
+    }
+    update_size(ptr);
+    return ptr;
+}
+
+template <typename T>
+void Tree<T>::print(int level){
+    if (left){
+        left->print(level + 1);
+    }
+    for (int i = 0; i < level; i++){
+        std::cout << '\t';
+    }
+    std::cout << data << ',' << size << std::endl;
+    if (right){
+        right->print(level + 1);
     }
 }
 
 template <typename T>
-void Tree<T>::write_node(std::string path){
-    if(this->root){
-        std::ofstream file(path, std::ios::app);
-        if(file.is_open()){
-            this->root->write_tree(file);
-            file << std::endl;
-        }
-        else{
-            std::cout << "file " << path << " could not open" << std::endl;
-        }
+void Tree<T>::write(std::ofstream &file){
+    if (left){
+        left->write(file);
+    }
+    file << data << " ";
+    if (right){
+        right->write(file);
     }
 }
 
 template <typename T>
-TreeNode<T>* Tree<T>::insert(TreeNode<T>* root, T data){
-    if(!root){
-        return new TreeNode<T>(data);
+Tree<T> *Tree<T>::insert_root(Tree<T> *ptr, T data){
+    if (!ptr){
+        std::cout << "create\n";
+        return new Tree<T>(data);
     }
-    if(rand()%(root->size+1) == 0){
-        return insert_root(root, data);
-    }
-    if(root->data > data){
-        root->left = insert(root->left, data);
+    if (ptr->data > data){
+        ptr->left = insert_root(ptr->left, data);
+        update_size(ptr);
+        return rotate_right(ptr);
     }
     else{
-        root->right = insert(root->right, data);
-    }
-    root->size = update_size(root);
-    return root;
-}
-
-template <typename T>
-TreeNode<T>* Tree<T>::insert_root(TreeNode<T>* root, T data){
-    if(!root){
-        return new TreeNode<T>(data);
-    }
-    if(data < root->data){
-        root->left = insert_root(root->left, data);
-        return rotate_right(root);
-    }
-    else{
-        root->right = insert_root(root->right, data);
-        return rotate_left(root);
+        ptr->right = insert_root(ptr->right, data);
+        update_size(ptr);
+        return rotate_left(ptr);
     }
 }
 
 template <typename T>
-TreeNode<T>* Tree<T>::rotate_left(TreeNode<T>* ptr){
-    TreeNode<T>* temp = ptr->right;
-    if(!temp){
-        return ptr;
-    }
-    ptr->right = temp->left;
-    temp->left = ptr;
-    temp->size = ptr->size;
-    ptr->size = update_size(ptr);
-    return temp;
-}
-
-template <typename T>
-TreeNode<T>* Tree<T>::rotate_right(TreeNode<T>* ptr){
-    TreeNode<T>* temp = ptr->left;
-    if(!temp){
+Tree<T> *Tree<T>::rotate_right(Tree<T> *ptr){
+    std::cout << "rotate_right\n";
+    Tree<T> *temp = ptr->left;
+    if (!temp){
+        std::cout << "exit\n";
         return ptr;
     }
     ptr->left = temp->right;
     temp->right = ptr;
     temp->size = ptr->size;
-    ptr->size = update_size(ptr);
+    update_size(ptr);
     return temp;
 }
 
 template <typename T>
-T Tree<T>::get_size(TreeNode<T>* root){
-    if(!root){
-        return 0;
+Tree<T> *Tree<T>::rotate_left(Tree<T> *ptr){
+    std::cout << "rotate_left\n";
+    Tree<T> *temp = ptr->right;
+    if (!temp){
+        return ptr;
     }
-    return root->size;
+    ptr->right = temp->left;
+    temp->left = ptr;
+    temp->size = ptr->size;
+    update_size(ptr);
+    return temp;
 }
 
 template <typename T>
-int Tree<T>::update_size(TreeNode<T>* root){
-    return get_size(root->left) + get_size(root->right) + 1;
+int Tree<T>::get_size(Tree<T> *ptr){
+    if (!ptr){
+        return 0;
+    }
+    return ptr->size;
+}
+
+template <typename T>
+void Tree<T>::update_size(Tree<T> *ptr){
+    ptr->size = get_size(ptr->left) + get_size(ptr->right) + 1;
 }
 
 #endif
