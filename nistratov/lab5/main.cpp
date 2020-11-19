44c8138a00ea2bfab147d4635b138a3519e92ac4
Dmitry Nistratov <dm.nistratov.wk@gmail.com>
2020-11-19 15:59:36 +0300

    main reworked

--- nistratov/lab5/main.cpp
+++ nistratov/lab5/main.cpp
@@ -10,13 +10,20 @@ int main()
     int xs[] = {2, 3, 7, 2, 8, 2,2,3,1,2,5,453464,6,4 ,754,745,67 ,567,5 ,756,7, 7,567,567,5,7};
     char cs[] = {'a', 'g', 's', 'b', 'e', 'c', 'a', 'w', 'd'};
     Treap<int> asd;
+    size_t n = 0;
+    std::cout << "Size: " << std::endl;
+    std::cin >> n;
+    int array[n];
+    std::cout << "Array: " << std::endl;
+    for (size_t i = 0; i < n; i++)
+        std::cin >> array[i];
 
     std::cout << "Inserting data:" << std::endl;
-    for (size_t t = 0; t < (sizeof(xs)/sizeof(xs[0])); t++)
+    for (size_t t = 0; t < (sizeof(array)/sizeof(array[0])); t++)
     {
         std::cout << "Data " << t << ":\nTime spend: ";
         auto start = std::chrono::high_resolution_clock::now();
-        asd.insert(asd.root, new Treap<int>(xs[t], rand()));
+        asd.insert(asd.root, new Treap<int>(array[t], rand()));
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start);
         std::cout << duration.count() << " nanoseconds" << std::endl;
