6167f51c1813ac40efbdc69919a8ceb3d642c9fc
EFFECT322 <55045221+EFFECT322@users.noreply.github.com>
2020-11-19 16:46:30 +0300

    Add files via upload

---
+++ nikolaev/lb3/BinaryTree.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <algorithm>
+#include "BinaryTree.h"
+
+template <typename base>
+BinaryTree<base>::BinaryTree()
+{
+	this->l = nullptr;
+	this->r = nullptr;
+}
+
+template <typename base>
+base BinaryTree<base>::RootBT()
+{
+	if (this == nullptr) { std::cerr << "Error: RootBT(null) \n"; exit(1); }
+	else return data;
+}
+
+void BinaryTree<char>::printBT() {
+	if (!this->IsNull()) {
+		if (!this->l->IsNull() && !this->r->IsNull() && !(this->RootBT() >= 'a' && this->RootBT() <= 'z'))
+			std::cout << '(';
+		this->l->printBT();
+		std::cout << this->RootBT();
+		this->r->printBT();
+		if (!this->l->IsNull() && !this->r->IsNull() && !(this->RootBT() >= 'a' && this->RootBT() <= 'z'))
+			std::cout << ')';
+	}
+}
+
+template <typename base>
+void BinaryTree<base>::destroy()
+{
+	if (this != NULL) {
+		this->l->destroy();
+		this->r->destroy();
+		delete this;
+	}
+}
+
+
+void BinaryTree<char>::change()
+{
+	if (!this->l->IsNull() && !this->r->IsNull())
+	{
+		this->l->change();
+		this->r->change();
+
+		/*if (this->l->IsNull() && this->r->IsNull())
+		{
+			if (this->data == '+' && this->l->data == this->r->data && this->l->data >= 'a' && this->l->data <= 'z' && this->r->data >= 'a' && this->r->data <= 'z')
+			{
+				this->data = '*';
+				this->l->data = '2';
+			}
+			else if (this->data == '+' && this->l->data == '*' && this->r->data == '*')
+			{
+				if (this->l->l->data >= '2' && this->l->l->data <= '8' && this->l->r->data >= 'a' && this->l->r->data <= 'z' && this->r->l->data >= '2' && this->r->l->data <= '8' && this->r->r->data >= 'a' && this->r->r->data <= 'z' && this->l->r->data == this->r->r->data)
+				{
+					this->data = '*';
+					this->l->data = (char)((((int)this->l->data - (int)48) * 2) + (int)48);
+					this->r->data = this->r->r->data;
+				}
+				else
+				{
+					std::cout << "Error! this->data >= 10! Can't transform expression!\n";
+				}
+			}
+		}*/
+	}
+	if (this->data == '+' && this->l->data == this->r->data && this->l->data >= 'a' && this->l->data <= 'z' && this->r->data >= 'a' && this->r->data <= 'z')
+	{
+		this->data = '*';
+		this->l->data = '2';
+	}
+	else if (this->data == '+' && this->l->data == '*' && this->r->data == '*')
+	{
+		if (this->l->l->data >= '2' && this->l->l->data < '8' && this->l->r->data >= 'a' && this->l->r->data <= 'z' && this->r->l->data >= '2' && this->r->l->data < '8' && this->r->r->data >= 'a' && this->r->r->data <= 'z' && this->l->r->data == this->r->r->data)
+		{
+			this->data = '*';
+			this->l->data = (char)((((int)this->l->l->data - (int)48) * 2) + (int)48);
+			this->r->data = this->r->r->data;
+			this->l->l->destroy();
+			this->l->r->destroy();
+			this->l->l = nullptr;
+			this->l->r = nullptr;
+			this->r->l->destroy();
+			this->r->r->destroy();
+			this->r->l = nullptr;
+			this->r->r = nullptr;
+		}
+		else
+		{
+			std::cout << "Error! this->data >= 10! Can't transform expression!\n";
+		}
+	}
+
+	/*if (!this->l->IsNull() && !this->r->IsNull())
+	{
+		if (this->data == '+' && this->l->data == this->r->data && this->l->data >= 'a' && this->l->data <= 'z' && this->r->data >= 'a' && this->r->data <= 'z')
+		{
+			this->data = '*';
+			this->l->data = '2';
+		}
+		this->l->change();
+		this->r->change();
+	}*/
+}
+
+void BinaryTree<char>::createBT(std::string& tmp)
+{
+	if (!tmp.empty())
+	{
+		if (this->l->IsNull() && this->r->IsNull())
+		{
+			if (tmp.front() >= 'a' && tmp.front() <= 'z')
+			{
+				this->data = tmp.front();
+				tmp.erase(0, 1);
+				this->l = new BinaryTree;
+				this->r = new BinaryTree;
+			}
+			else
+			{
+				this->data = tmp.front();
+				tmp.erase(0, 1);
+				this->l = new BinaryTree;
+				this->r = new BinaryTree;
+				this->l->createBT(tmp);
+				this->r->createBT(tmp);
+			}
+		}
+		else
+		{
+			this->l->createBT(tmp);
+			this->r->createBT(tmp);
+		}
+	}
+}
+
+template <typename base>
+bool BinaryTree<base>::IsNull()
+{
+	return (this == nullptr);
+}
+
+template <typename base>
+BinaryTree<base>::~BinaryTree()
+{
+}
