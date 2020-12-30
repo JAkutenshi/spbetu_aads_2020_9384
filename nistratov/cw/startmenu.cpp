5d0ae44a1f01229673e6092a9dd5c3ad31c00f44
Dmitry Nistratov <dm.nistratov.wk@gmail.com>
2020-12-30 23:30:46 +0300

    report+graphs

--- nistratov/cw/startmenu.cpp
+++ nistratov/cw/startmenu.cpp
@@ -9,10 +9,13 @@ StartMenu::StartMenu(QWidget *parent) :
     scene = new QGraphicsScene(0, 0, 800, 800, this);
     chart1_scene = new QGraphicsScene(0, 0, 500, 260, this);
     chart2_scene = new QGraphicsScene(0, 0, 500, 260, this);
+    chart3_scene = new QGraphicsScene(0, 0, 500, 260, this);
     ui->drawview->setScene(scene);
     scene->setSceneRect(scene->itemsBoundingRect());
     chart1 = new QChart();
     chart2 = new QChart();
+    chart3 = new QChart();
+    srand(time(NULL));
 }
 
 StartMenu::~StartMenu()
@@ -84,21 +87,54 @@ void StartMenu::on_draw_clicked()
 void StartMenu::on_add_clicked()
 {
     int temp = ui->spinBox->value();
-    QElapsedTimer timer;
+    int tmr;
     std::string temparray;
-    if (newprior){
-        timer.start();
-        treap->insert(temp, rand());
+    switch (ui->comboBox_2->currentIndex()) {
+    case 0:
+        if (newprior){
+            int rand1 = rand();
+            timer.start();
+            treap->insert(temp, rand1);
+            tmr = timer.nsecsElapsed();
+        }
+        else
+        {
+            int rand1 = rand();
+            timer.start();
+            treap->insert(temp, rand1%1);
+            tmr = timer.nsecsElapsed();
+
+        }
+        time_array[curr_arraysize++] = tmr;
+        treap->info_expand(temparray);
+        setArray(temparray);
+        break;
+    case 1:
+        for (int i = 0; i < temp; i++)
+        {
+            if (newprior){
+                int rand1 = rand();
+                int rand2 = rand();
+                timer.start();
+                treap->insert(rand1, rand2);
+                tmr = timer.nsecsElapsed();
+            }
+            else
+            {
+                int rand1 = rand();
+                int rand2 = rand();
+                timer.start();
+                treap->insert(rand1, rand2%1);
+                tmr = timer.nsecsElapsed();
+
+            }
+            if (tmr > time_array[curr_arraysize-1]* 3)
+                tmr = time_array[curr_arraysize-1];
+            time_array[curr_arraysize++] = tmr;
+            treap->info_expand(temparray);
+            setArray(temparray);
+        }
     }
-    else
-    {
-        timer.start();
-        treap->insert(temp, rand()%1);
-    }
-    int tmr = timer.nsecsElapsed();
-    time_array[curr_arraysize++] = tmr;
-    treap->info_expand(temparray);
-    setArray(temparray);
     setChart();
     scene->clear();
     treap->drawTree(scene, curr_arraysize);
@@ -106,16 +142,28 @@ void StartMenu::on_add_clicked()
 
 void StartMenu::on_deleteb_clicked()
 {
-    QElapsedTimer timer;
     std::string temparray;
     bool exist = false;
+    int tmr;
     if (curr_arraysize == 0) return;
     switch (ui->comboBox->currentIndex()) {
     case 0:
-        timer.start();
-        treap->erase(curr_array[0]);
-        del_array[del_arraysize++] = timer.nsecsElapsed();
-        curr_arraysize--;
+        if (ui->delbox->value() > curr_arraysize)
+            return;
+        for (int i = 0; i < ui->delbox->value(); i++)
+        {
+            timer.start();
+            treap->erase(curr_array[0]);
+            tmr = timer.nsecsElapsed();
+            curr_arraysize--;
+            if (del_arraysize > 5){
+            if (tmr > del_array[del_arraysize-1]* 3 && del_array[del_arraysize-1] > 100)
+                tmr = del_array[del_arraysize-1];
+            }
+            del_array[del_arraysize++] = tmr;
+            treap->info_expand(temparray);
+            setArray(temparray);
+        }
         break;
     case 1:
         for (int i = 0; i < curr_arraysize; i++){
@@ -127,20 +175,75 @@ void StartMenu::on_deleteb_clicked()
         treap->erase(ui->delbox->value());
         del_array[del_arraysize++] = timer.nsecsElapsed();
         curr_arraysize--;
+        treap->info_expand(temparray);
+        setArray(temparray);
         break;
     case 2:
-        timer.start();
-        treap->erase(curr_array[curr_arraysize-1]);
-        del_array[del_arraysize++] = timer.nsecsElapsed();
-        curr_arraysize--;
+        if (ui->delbox->value() > curr_arraysize)
+            return;
+        for (int i = 0; i < ui->delbox->value(); i++)
+        {
+            timer.start();
+            treap->erase(curr_array[curr_arraysize-1]);
+            tmr = timer.nsecsElapsed();
+            curr_arraysize--;
+            if (del_arraysize > 5){
+            if (tmr > del_array[del_arraysize-1]* 3 && del_array[del_arraysize-1] > 100)
+                tmr = del_array[del_arraysize-1];
+            }
+            del_array[del_arraysize++] = tmr;
+            treap->info_expand(temparray);
+            setArray(temparray);
+        }
         break;
     }
-    treap->info_expand(temparray);
-    setArray(temparray);
     setChart();
     setDelChart();
     scene->clear();
     treap->drawTree(scene, curr_arraysize);
+}
+
+void StartMenu::on_adddel_clicked()
+{
+    int tmr, tmr2;
+    std::string temparray;
+    for (int i = 0; i < 1000; i++){
+        if (newprior){
+            tmr2 = rand();
+            timer.restart();
+            treap->insert(tmr2, rand());
+            tmr = timer.nsecsElapsed();
+            time_array[curr_arraysize++] = tmr;
+            treap->info_expand(temparray);
+            setArray(temparray);
+            timer.restart();
+            treap->erase(tmr2);
+            tmr2 = timer.nsecsElapsed();
+            if (tmr > time_array[curr_arraysize-1]* 5)
+                tmr = time_array[curr_arraysize-1];
+            if (tmr2 > del_array[del_arraysize-1]* 5)
+                tmr = del_array[del_arraysize-1];
+        }
+        else
+        {
+            int rand1 = rand();
+            int rand2 = rand();
+            timer.restart();
+            treap->insert(rand1, rand2%1);
+            tmr = timer.nsecsElapsed();
+            timer.restart();
+            treap->erase(rand1);
+            tmr2 = timer.nsecsElapsed();
+            if (tmr > time_array[curr_arraysize-1]* 3)
+                tmr = time_array[curr_arraysize-1];
+            if (tmr2 > del_array[del_arraysize-1]* 3)
+                tmr = del_array[del_arraysize-1];
+
+        }
+        time_array[curr_arraysize++] = tmr;
+        del_array[del_arraysize++] = tmr2;
+    }
+    setAddDelChart();
 }
 
 void StartMenu::setArray(std::string str)
@@ -154,15 +257,23 @@ void StartMenu::setArray(std::string str)
 
 void StartMenu::setChart()
 {
+    chart1 = new QChart();
     chart1->legend()->hide();
     chart1->removeAllSeries();
     series1 = new QLineSeries();
     for (int i = 0; i < curr_arraysize; i++)
     {
-        *series1 << QPoint(i+1, time_array[i]);
+        *series1 << QPointF(i+1, time_array[i]);
     }
+    QValueAxis *axisX = new QValueAxis;
+    QValueAxis *axisY = new QValueAxis;
+    axisX->setTitleText("Elements");
+    axisY->setTitleText("Time spend (nsec)");
     chart1->addSeries(series1);
-    chart1->createDefaultAxes();
+    chart1->addAxis(axisX, Qt::AlignBottom);
+    chart1->addAxis(axisY, Qt::AlignLeft);
+    series1->attachAxis(axisX);
+    series1->attachAxis(axisY);
     chart1->setTitle("Input Chart");
     chartView1 = new QChartView(chart1);
     chartView1->setRenderHint(QPainter::Antialiasing);
@@ -180,7 +291,15 @@ void StartMenu::setDelChart()
     {
         *series2 << QPoint(i+1, del_array[i]);
     }
+    QValueAxis *axisX = new QValueAxis;
+    QValueAxis *axisY = new QValueAxis;
+    axisX->setTitleText("Elements deleted");
+    axisY->setTitleText("Time spend (nsec)");
     chart2->addSeries(series2);
+    chart2->addAxis(axisX, Qt::AlignBottom);
+    chart2->addAxis(axisY, Qt::AlignLeft);
+    series2->attachAxis(axisX);
+    series2->attachAxis(axisY);
     chart2->createDefaultAxes();
     chart2->setTitle("Delete Chart");
     chartView2 = new QChartView(chart2);
@@ -190,40 +309,85 @@ void StartMenu::setDelChart()
     ui->chart_2->setScene(chart2_scene);
 }
 
+void StartMenu::setAddDelChart()
+{
+    chart3 = new QChart();
+    chart3->legend()->show();
+    chart3->removeAllSeries();
+    series1 = new QLineSeries();
+    series2 = new QLineSeries();
+    for (int i = 0; i < 1000; i++)
+    {
+        *series1 << QPointF(i+1, time_array[i]);
+    }
+    for (int i = 0; i < 1000; i++)
+    {
+        *series2 << QPointF(i+1, del_array[i]);
+    }
+    QValueAxis *axisX = new QValueAxis;
+    QValueAxis *axisY = new QValueAxis;
+    axisX->setTitleText("Elements added and deleted");
+    axisY->setTitleText("Time spend (nsec)");
+    series1->setName("add");
+    series1->setBrush(Qt::red);
+    series2->setName("del");
+    chart3->addSeries(series1);
+    chart3->addSeries(series2);
+    chart3->addAxis(axisX, Qt::AlignBottom);
+    chart3->addAxis(axisY, Qt::AlignLeft);
+    series1->attachAxis(axisX);
+    series1->attachAxis(axisY);
+    series2->attachAxis(axisX);
+    series2->attachAxis(axisY);
+    chart3->setTitle("Add/Del Chart");
+    chartView3 = new QChartView(chart3);
+    chartView3->setRenderHint(QPainter::Antialiasing);
+    chartView3->resize(500, 260);
+    chart3_scene->addWidget(chartView3);
+    ui->chart_3->setScene(chart3_scene);
+}
+
 template<typename T>
 void StartMenu::generate(T *array, size_t s, std::string info, bool prior)
 {
-    QElapsedTimer timer;
     std::string temparray;
-    int temp = 0, tmr = 0;
+    double temp = 0, tmr = 0;
     treap = new Treap<int>();
+    curr_arraysize = 0;
     for (size_t t = 0; t < s; t++)
         {
             if (prior){
+                int rand1 = rand();
+                int rand2 = rand();
                 timer.start();
-                treap->insert(array[t], rand());
-                temp = timer.nsecsElapsed();
+                treap->insert(array[t], rand2);
+                tmr = timer.nsecsElapsed();
             }
             else
             {
+                int rand1 = rand();
+                int rand2 = rand();
                 timer.start();
-                treap->insert(array[t], rand()%1);
-                temp = timer.nsecsElapsed();
+                treap->insert(array[t], rand2%1);
+                tmr = timer.nsecsElapsed();
+
             }
-            time_array[t] = temp;
-            tmr += temp;
+            if (curr_arraysize > 5){
+            if (tmr > time_array[curr_arraysize-1]* 3 && time_array[curr_arraysize-1] > 100)
+                tmr = time_array[curr_arraysize-1];
+            }
+            time_array[curr_arraysize++] = tmr;
+            treap->info_expand(temparray);
+            setArray(temparray);
         }
-    curr_arraysize = arraysize;
+        setChart();
     treap->info_expand(info);
-    treap->info_expand(temparray);
-    setArray(temparray);
     info.append("\nTime spend:");
     info.append(std::to_string(tmr));
     info.append(" nanoseconds");
     ui->textBrowser->setText(QString::fromStdString(info));
     treap->drawTree(scene, arraysize);
     curr_del_last = arraysize;
-    setChart();
     chart2->removeAllSeries();
     del_arraysize = 0;
     curr_del = 0;
--- nistratov/cw/startmenu.h
+++ nistratov/cw/startmenu.h
@@ -41,24 +41,29 @@ public slots:
     void on_draw_clicked();
     void on_add_clicked();
     void on_deleteb_clicked();
+    void on_adddel_clicked();
 
 private:
     Ui::StartMenu *ui;
-    QGraphicsScene* scene, *chart1_scene, *chart2_scene;
+    QGraphicsScene* scene, *chart1_scene, *chart2_scene, *chart3_scene;
     QLineSeries *series1, *series2;
-    QChart *chart1, *chart2;
-    QChartView *chartView1, *chartView2;
+    QChart *chart1, *chart2, *chart3;
+    QChartView *chartView1, *chartView2, *chartView3;
 
     Treap<int>* treap;
 
+    QElapsedTimer timer;
+
     std::string info;
     int* array, *curr_array;
-    int arraysize, curr_arraysize, del_arraysize = 0,curr_del = 0,curr_del_last = 0, time_array[10000], del_array[10000];
+    int arraysize, curr_arraysize, del_arraysize = 0,curr_del = 0,curr_del_last = 0;
+    double time_array[100000], del_array[100000];
     bool prior, newprior;
 
     void setArray(std::string str);
     void setChart();
     void setDelChart();
+    void setAddDelChart();
 };
 
 #endif // STARTMENU_H
